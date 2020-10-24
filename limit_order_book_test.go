f1b22f65bdeb47de537d99d944f6590b5002e1d6
Robert Davis <rtdavis22@gmail.com>
2020-10-23 20:23:03 -0700

    Clean up limit order book test

--- limit_order_book_test.go
+++ limit_order_book_test.go
@@ -7,75 +7,55 @@ import (
 )
 
 func TestSinglePriceCall(t *testing.T) {
-	exchange := NewExchange()
-	exchange.CreateSecurity(XOM)
+	book := NewLimitOrderBook()
 
 	// From Trading & Exchanges Chapter 6
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   3,
-		limit:  20.0,
-		type_:  ORDER_TYPE_BUY,
+	book.AddBuyOrder(LimitOrder{
+		size:  3,
+		limit: 20.0,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   2,
-		limit:  20.1,
-		type_:  ORDER_TYPE_SELL,
+	book.AddSellOrder(LimitOrder{
+		size:  2,
+		limit: 20.1,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   2,
-		limit:  20.0,
-		type_:  ORDER_TYPE_BUY,
+	book.AddBuyOrder(LimitOrder{
+		size:  2,
+		limit: 20.0,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   1,
-		limit:  19.8,
-		type_:  ORDER_TYPE_SELL,
+	book.AddSellOrder(LimitOrder{
+		size:  1,
+		limit: 19.8,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   5,
-		limit:  20.2,
-		type_:  ORDER_TYPE_SELL,
+	book.AddSellOrder(LimitOrder{
+		size:  5,
+		limit: 20.2,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   4,
-		limit:  20.3,
-		type_:  ORDER_TYPE_BUY,
+	book.AddBuyOrder(LimitOrder{
+		size:  4,
+		limit: 20.3,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   2,
-		limit:  20.1,
-		type_:  ORDER_TYPE_BUY,
+	book.AddBuyOrder(LimitOrder{
+		size:  2,
+		limit: 20.1,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   6,
-		limit:  20.0,
-		type_:  ORDER_TYPE_SELL,
+	book.AddSellOrder(LimitOrder{
+		size:  6,
+		limit: 20.0,
 	})
 
-	exchange.SubmitLimitOrder(LimitOrderRequest{
-		symbol: XOM,
-		size:   7,
-		limit:  19.8,
-		type_:  ORDER_TYPE_BUY,
+	book.AddBuyOrder(LimitOrder{
+		size:  7,
+		limit: 19.8,
 	})
 
-	security := exchange.GetSecurity(XOM)
-	callResult := security.SinglePriceCall()
+	callResult := book.SinglePriceCall()
 
 	assert.InDelta(t, 20.0, callResult.Price, 0.01)
 	assert.InDelta(t, 1.6, callResult.TraderSurplus, 0.01)
